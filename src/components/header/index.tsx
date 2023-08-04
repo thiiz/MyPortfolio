b64ee379287a7899024c3d8af31c5f23b100da7b
thiizz <afffvitu@gmail.com>
2023-08-04 06:11:56 -0300

    header absolute

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,7 @@ import { LangSwitcher } from './lang_switcher'
 import { LangProps } from '@/@types/LangProps'
 import { getDictionary } from '@/utils/getDictionary'
 import { navRoutes } from '@/routes'
+
 interface HeaderProps {
   lang: LangProps
 }
@@ -12,7 +13,7 @@ export const Header = async ({ lang }: HeaderProps) => {
   const dictionary = await getDictionary(lang)
   const { routes } = navRoutes({ dictionary })
   return (
-    <header className='container mx-auto px-4 py-2'>
+    <header className='bg-gray-950 container mx-auto px-4 py-2 z-50 absolute top-0 left-0'>
       <div className='flex justify-between '>
         <div className='flex items-center gap-1.5'>
           <Logo.Icon />
