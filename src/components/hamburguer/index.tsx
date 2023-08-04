6a4699da935c1b64b32d42a92c34bae9605113a3
thiizz <afffvitu@gmail.com>
2023-08-04 01:41:19 -0300

    mobile hamburguer button

--- src/components/hamburguer/index.tsx
+++ src/components/hamburguer/index.tsx
@@ -1,22 +1,21 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface ButtonHamburguerProps {
-
+interface ButtonHamburguerProps extends ComponentProps<'button'> {
+  isOpen: boolean;
 }
 
-const ButtonHamburguer: FC<ButtonHamburguerProps> = () => {
+const ButtonHamburguer: FC<ButtonHamburguerProps> = ({ isOpen, ...rest }) => {
   return (
-    <div>
-      <button className="relative group">
-        <div className="relative flex items-center justify-center rounded-full w-11 h-11 transform transition-all bg-slate-700 ring-0 ring-gray-50 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
-          <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 group-focus:-rotate-[45deg] origin-center">
-            <div className="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75 group-focus:-translate-y-[1px]"></div>
-            <div className="bg-white h-[1px] rounded"></div>
-            <div className="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75 group-focus:translate-y-[1px]"></div>
-          </div>
+    <button {...rest} className="relative group min-h[2.5rem] block lg:hidden">
+      <div className={twMerge("relative flex items-center justify-center rounded-full w-10 h-10 transform transition-all bg-slate-700 ring-0 ring-gray-50 hover:ring-8 ring-opacity-30 duration-200 shadow-md", isOpen ? "ring-4" : "")}>
+        <div className={twMerge("flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center", isOpen ? "-rotate-[45deg]" : "")}>
+          <div className={twMerge("bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 origin-right delay-75", isOpen ? "-rotate-90 h-[1px] -translate-y-[1px]" : "")}></div>
+          <div className="bg-white h-[1px] rounded"></div>
+          <div className={twMerge("bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 origin-left delay-75", isOpen ? "-rotate-90 h-[1px] translate-y-[1px]" : "")}></div>
         </div>
-      </button>
-    </div>
+      </div>
+    </button>
   );
 }
 
