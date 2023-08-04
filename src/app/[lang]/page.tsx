16cfaea09f273e7f7be629fd27d210b7974b00f9
thiizz <afffvitu@gmail.com>
2023-08-04 06:12:06 -0300

    update

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,34 +1,45 @@
 import { LangProps } from "@/@types/LangProps";
+import Ellipses from "@/components/ellipses";
 import { getDictionary } from "@/utils/getDictionary";
+import Image from "next/image";
+import image from '@/../public/person.png'
+
 
 export default async function Home({ params }: { params: { lang: LangProps } }) {
   const dictionary = await getDictionary(params.lang);
-  const {
-    Vítor_is_a,
-    web_designer,
-    and,
-    frontend_developer
-  } = dictionary.Vítor_is_a_web_designer_and_frontend_developer
+  const title = dictionary.Vítor_is_a_web_designer_and_frontend_developer
+  const subtitle = dictionary.he_crafts_responsive_websites_where_technologies_meet_creativity
 
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl font-semibold break-words">
         <span className="text-white">
-          {Vítor_is_a}
+          {title.Vítor_is_a}
           {' '}
         </span>
         <span className="text-primary">
-          {web_designer}
+          {title.web_designer}
           {' '}
         </span>
         <span className="text-white">
-          {and}
+          {title.and}
           {' '}
         </span>
         <span className="text-primary">
-          {frontend_developer}
+          {title.frontend_developer}
         </span>
       </h1>
+      <h2 className="text-gray-50 mt-6  ">{subtitle}</h2>
+      <div className="relative max-w-xl w-full h-full">
+        <Image
+          src={image}
+          alt="vítor"
+          width={0}
+          height={0}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="w-full object-cover" />
+          <Ellipses number={5} />
+      </div>
     </div>
   )
 }
