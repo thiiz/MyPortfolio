21fb9a9c4c2ea4edaf945d88225067460b51717a
thiizz <afffvitu@gmail.com>
2023-08-04 01:39:19 -0300

    get dictionaries hook

--- src/utils/getDictionary.ts
+++ src/utils/getDictionary.ts
@@ -1,9 +1,9 @@
-const dictionaries: any = {
+const dictionaries = {
   en: () => import("@/dictionaries/en.json").then(r => r.default),
   pt: () => import("@/dictionaries/pt.json").then(r => r.default),
   es: () => import("@/dictionaries/es.json").then(r => r.default),
 }
 
-export const getDictionary = (lang: string) => {
+export const getDictionary = (lang: keyof typeof dictionaries) => {
   return dictionaries[lang]()
 }
